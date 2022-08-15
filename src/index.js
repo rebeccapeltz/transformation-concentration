9b686112080ed8ab579a0aa4ffcf389d1be425ed
rebeccapeltz <rebeccapeltz@gmail.com>
2022-08-15 15:28:30 -0700

    add media queries and cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// import { StrictMode } from "react";
-// import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 
 
@@ -7,13 +5,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from "./App";
 
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-//   rootElement
-// );
+
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container); 
 root.render(<App tab="home" />);
